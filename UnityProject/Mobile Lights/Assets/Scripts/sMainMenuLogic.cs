916f192e2f1176f17e1fbc241b2c104fab059f4d
gbh101 <garretthux@gmail.com>
2017-06-01 13:10:31 -0700

    Main Menu Logic
    Set up the main menu and confirmation of destructive action

---
+++ UnityProject/Mobile Lights/Assets/Scripts/sMainMenuLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sMainMenuLogic : MonoBehaviour
+{
+    // CoDA Prefab
+    public GameObject ConfirmationBox;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("BlankScene");
+        // Load play space
+    }
+
+    public void HowToPlay()
+    {
+        // Load HTP Screen
+    }
+
+    public void QuitGame()
+    {
+        // Creates a CoDA dialog box from prefab
+        Instantiate(ConfirmationBox, new Vector3(0,0,-20), Quaternion.identity);
+    }
+}
