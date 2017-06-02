aa245d9e7c87599f09e3b0c9fcfc7cbd954be7a3
gbh101 <garretthux@gmail.com>
2017-06-01 19:15:27 -0700

    Working on Event System

---
+++ UnityProject/Mobile Lights/Assets/sEventManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class sEventManager : MonoBehaviour
+{
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    private static sEventManager eventManager;
+    /*
+    public static sEventManager instance
+    {
+        get
+        {
+            if(!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(sEventManager)) as sEventManager;
+                
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManager script on a GameObject in a scene");
+                }
+            }
+        }
+    }
+    */
+}
---
+++ UnityProject/Mobile Lights/Assets/sGameplayCanvasLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sGameplayCanvasLogic : MonoBehaviour
+{
+    public void ResetLevel()
+    {
+        // Bring up CoDA to Reset level
+            // Resets the board state to its initial setting
+            // Maintains # of clicks through reset
+    }
+
+    public void HTP()
+    {
+        // Instantiate the HTP canvas
+    }
+
+    public void LevelSelect()
+    {
+        // Bring up CoDA to Level Select scene
+    }
+
+    public void MainMenu()
+    {
+        // Bring up CoDA to Main Menu scene
+    }
+}
