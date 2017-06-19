9dc19181785a569be407bd89a85ca5d4b0a5ea73
Unknown <garretthux@gmail.com>
2017-06-19 14:16:48 -0700

    Shape Enums
    Created enums for each of the basic shapes for use in visually creating levels.
    Need to add shape sprites into engine and hook them up to Enum system.

---
+++ UnityProject/Mobile Lights/Assets/Scripts/sTileLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType {Square,Plus,X,TriangleUp,TriangleDown,TriangleLeft,
+                        TriangleRight,VerticalLine,HorizontalLine,SlashLeft,
+                        SlashRight,Generic,NoLight};
+
+public class sTileLogic : MonoBehaviour {
+
+    public enum tileType { square, plus, x, triangleUp, triangleDown, triangleLeft,
+                            triangleRight, verticalLine, horizontalLine, slashLeft,
+                            slashRight, generic, noLight };
+
+    public tileType TileEnum;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
