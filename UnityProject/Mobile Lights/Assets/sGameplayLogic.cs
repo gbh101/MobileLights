6bfbc871a4714e2514d4c38989994767be106fb4
Unknown <garretthux@gmail.com>
2017-08-01 13:24:21 -0700

    Start of Gameplay Logic

---
+++ UnityProject/Mobile Lights/Assets/sGameplayLogic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sGameplayLogic : MonoBehaviour
+{
+    private int iNumberOfLights = 0;
+    private int iNumberLitUp = 0;
+
+    private int iNumberOfClicks = 0;
+        
+	void Awake ()
+    {
+        // Loop through all existing lights\
+        //{
+            // iNumberOfLights += 1;
+            
+            // if(a light is on by default)
+            //{
+                // iNumberLitUp += 1;
+                // i++;
+            //}
+        //}
+	}
+
+    private void LightOn()
+    {
+        // iNumberLitUp += 1;
+        // Change display on UI
+
+        // if(iNumberLitUp == iNumberOfLights)
+            //{
+                // Spawn victory image
+            //}
+    }
+
+    private void LightOff()
+    {
+        // iNumberLitUp -= 1;
+        // Change display on UI
+    }
+
+    private void Click()
+    {
+        // iNumberOfClicks += 1;
+    }
+}
